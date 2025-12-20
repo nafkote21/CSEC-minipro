e0caab06d1ca450e60522881378ce24c0dd214e1
meaza <nafkotemaye2013m@gmail.com>
2025-12-20 21:13:24 +0300

    checked and uncked button is added

--- project 4 Todo Loost App/script.js
+++ project 4 Todo Loost App/script.js
@@ -1,12 +1,14 @@
-const image= document.getElementById("my");
+const image= document.querySelectorAll("img#my");
 let ischecked= false;
 
-image.addEventListener("click", function() {
-    if(!ischecked){
-        image.src="check.png";
+image.forEach(function(img) {
+    img.addEventListener("click", function() {
+        if(!ischecked){
+            img.src="check.png";
         ischecked=true;
     } else {
-        image.src="uncheck.png";
+        img.src="uncheck.png";
         ischecked=false;
     }
+});
 });
