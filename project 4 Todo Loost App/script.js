f5cf77aabbdf9563a28ce603d0d8f312ccfea5c7
meaza <nafkotemaye2013m@gmail.com>
2025-12-21 20:26:12 +0300

    done project 4

--- project 4 Todo Loost App/script.js
+++ project 4 Todo Loost App/script.js
@@ -1,14 +1,39 @@
-const image= document.querySelectorAll("img#my");
-let ischecked= false;
+const taskForm = document.querySelector('.todo-form');
+const taskInput = document.querySelector('.taskinput');
+const taskList = document.querySelector('.taskList');
 
-image.forEach(function(img) {
-    img.addEventListener("click", function() {
-        if(!ischecked){
-            img.src="check.png";
-        ischecked=true;
-    } else {
-        img.src="uncheck.png";
-        ischecked=false;
+taskForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const taskText = taskInput.value.trim();
+    if(taskText === "") return;
+
+    const li = document.createElement('li');
+    li.classList.add('task');
+
+    li.innerHTML = `
+        <img src="uncheck.png" class="checkbox">
+        <span class="task-text">${taskText}</span>
+        <button class="delete-btn">X</button>
+    `;
+
+    taskList.appendChild(li);
+    taskInput.value = "";
+});
+
+taskList.addEventListener('click', function(e) {
+    if(e.target.classList.contains('checkbox')) {
+        const img = e.target;
+        if(img.src.includes("uncheck.png")) {
+            img.src = "check.png";
+        } else {
+            img.src = "uncheck.png";
+        }
     }
 });
+
+taskList.addEventListener('click', function(e) {
+    if(e.target.classList.contains('delete-btn')) {
+        e.target.parentElement.remove();
+    }
 });
